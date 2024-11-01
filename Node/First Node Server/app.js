9ca607abef70c170073299b583c72e0529a3f95a
AYUSH KUMAR SINGH <145869549+ayuwusshh@users.noreply.github.com>
2024-11-02 02:46:43 +0530

    requests

--- Node/First Node Server/app.js
+++ Node/First Node Server/app.js
@@ -1,7 +1,7 @@
 const http = require('http');
 const server = http.createServer(function (req, res) {
   // console.log('Request Headers:', req.headers);
-  console.log('req');
+  console.log(req.url, req.method, req.headers);
   // process.exit();
 });
 const PORT = 3000;
