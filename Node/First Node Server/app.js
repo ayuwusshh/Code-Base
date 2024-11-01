ad0579a9fcab7f1746e07ac6ad89a794a758b9ed
AYUSH KUMAR SINGH <145869549+ayuwusshh@users.noreply.github.com>
2024-11-02 03:24:15 +0530

    url

--- Node/First Node Server/app.js
+++ Node/First Node Server/app.js
@@ -3,12 +3,18 @@ const server = http.createServer(function (req, res) {
   // console.log('Request Headers:', req.headers);
   console.log(req.url, req.method, req.headers);
   // process.exit();
-  res.setHeader('Content-Type','text/html')
-  res.write('<html>');
-  res.write('<head><title>Ayush SIngh Rajput<title><head>');
-  res.write('<body><h1>Welcome<h1><body>');
-  res.write('<html>');
-  res.end();
+  if (req.url === '/') {
+
+  } else if () {
+
+  } else {
+    res.setHeader('Content-Type', 'text/html')
+    res.write('<html>');
+    res.write('<head><title>Ayush SIngh Rajput</title></head>');
+    res.write('<body><h1>Welcome</h1></body>');
+    res.write('<html>');
+    res.end();
+  }
 });
 const PORT = 3000;
 server.listen(PORT, () => {
