3be2bee7400f5cab3db7bfec9b0bed2de98a648e
AYUSH KUMAR SINGH <145869549+ayuwusshh@users.noreply.github.com>
2024-11-20 00:27:45 +0530

    lol

--- Striver/arrays/rotate_k.cpp
+++ Striver/arrays/rotate_k.cpp
@@ -1,5 +1,17 @@
-#include<iostream>
+#include <iostream>
 using namespace std;
-int main(){
-  
+int main()
+{
+  int arr[] = {1, 2, 3, 4, 5, 6, 7};
+  int k = 3;
+  int n = sizeof(arr) / sizeof(arr[0]);
+  int temp = (arr[0], arr[1], arr[2]);
+  for (int i = k; i < n; i++)
+  {
+    arr[i] = arr[i - k];
+  }
+  for (int j = k; j < n; j++)
+  {
+    arr[i] = temp[j];
+  }
 }
