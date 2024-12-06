5f9c87244d259dabb0ab3d7ecebfffdbb611de6d
AYUSH KUMAR SINGH <145869549+ayuwusshh@users.noreply.github.com>
2024-12-07 02:04:43 +0530

    m

--- c++/praccc/smallest.cpp
+++ c++/praccc/smallest.cpp
@@ -1,9 +1,28 @@
 #include <iostream>
 using namespace std;
+
 int main()
 {
-  int large=INT_MIN;
-  int small=INT_MAX;
+  int n = 5;
   int arr[] = {5, 11, 2, -8, 7};
-  for
+
+  int minIndex = 0;
+  int maxIndex = 0;
+
+  for (int i = 1; i < n; i++)
+  {
+    if (arr[i] < arr[minIndex])
+    {
+      minIndex = i;
+    }
+    if (arr[i] > arr[maxIndex])
+    {
+      maxIndex = i;
+    }
+  }
+
+  cout << "Index of minimum element: " << minIndex << endl;
+  cout << "Index of maximum element: " << maxIndex << endl;
+
+  return 0;
 }
