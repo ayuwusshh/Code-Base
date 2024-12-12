b9cd898c35464d57e44a90a13ef84eb17faf5c73
AYUSH KUMAR SINGH <145869549+ayuwusshh@users.noreply.github.com>
2024-12-12 11:40:15 +0530

    done

--- c++/praccc/linearSearch.cpp
+++ c++/praccc/linearSearch.cpp
@@ -1,17 +1,21 @@
-#include<iostream>
+#include <iostream>
 using namespace std;
-int linearSearch(int arr[],int size,int target){
+int linearSearch(int arr[], int size, int target)
+{
 
-  for(int i=0;i<size;i++){
-    if(arr[i]==target){
-      cout<< i;
+  for (int i = 0; i < size; i++)
+  {
+    if (arr[i] == target)
+    {
+      return i;
     }
   }
   return -1;
 }
-int main(){
-  int arr[]={4,2,7,1,2,8,5};
-  int size=7;
-  int target=1;
-  linearSearch(arr,size,target);
+int main()
+{
+  int arr[] = {4, 2, 7, 1, 2, 8, 5};
+  int size = 7;
+  int target = 1;
+  cout << linearSearch(arr, size, target);
 }
