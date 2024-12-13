0196c6359bc77bf8ce363e895f7c212bf2656561
AYUSH KUMAR SINGH <145869549+ayuwusshh@users.noreply.github.com>
2024-12-13 17:39:33 +0530

    subArray

---
+++ SHRADHA DIDI/ARRAYS/SubArrays.cpp
@@ -0,0 +1,20 @@
+#include <iostream>
+using namespace std;
+int main()
+{
+  int arr[] = {9, 5, 6, 8, 7};
+  int n = 5;
+  for (int st = 0; st < n; st++)
+  {
+    for (int end = st; end < n; end++)
+    {
+      for (int i = st; i <= end; i++)
+      {
+        cout << arr[i];
+      }
+      cout << " ";
+    }
+    cout << endl;
+  }
+  return 0;
+}
