7faf6af8da61aeb6b69c4b37da096d570dd37765
AYUSH KUMAR SINGH <145869549+ayuwusshh@users.noreply.github.com>
2024-12-14 17:08:48 +0530

    pairSum

--- SHRADHA DIDI/ARRAYS/kadanesAlgorithm.cpp
+++ SHRADHA DIDI/ARRAYS/kadanesAlgorithm.cpp
@@ -1,9 +1,22 @@
+// kadanesAlgorithm
+
 #include <iostream>
 using namespace std;
 int main()
 {
   int arr[] = {3, -4, 5, 4, -1, 7, -8};
   int n = 7;
-
+  int maxSum = INT_MIN;
+  int currentSum = 0;
+  for (int i = 0; i < n; i++)
+  {
+    currentSum += arr[i];
+    maxSum = max(currentSum, maxSum);
+    if (currentSum < 0)
+    {
+      currentSum = 0;
+    }
+  }
+  cout << maxSum << endl;
   return 0;
-} 
+}
