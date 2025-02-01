d4bcb1537598b69d475991401fadd53d034d44e6
AYUSH KUMAR SINGH <145869549+ayuwusshh@users.noreply.github.com>
2025-02-01 16:30:51 +0530

    c

--- SHRADHA DIDI/ARRAYS/SubArrays.cpp
+++ SHRADHA DIDI/ARRAYS/SubArrays.cpp
@@ -1,22 +1,3 @@
-#include <iostream>
-using namespace std;
-int main()
-{
-  int arr[] = {9, 5, 6, 8, 7};
-  int n = 5;
-  for (int st = 0; st < n; st++)
-  {
-    for (int end = st; end < n; end++)
-    {
-      for (int i = st; i <= end; i++)
-      {
-        cout << arr[i];
-      }
-      cout << " ";
-    }
-    cout << endl;
-  }
-}
 // #include <iostream>
 // using namespace std;
 // int main()
@@ -35,5 +16,25 @@ int main()
 //     }
 //     cout << endl;
 //   }
-//   return 0;
 // }
+
+#include <iostream>
+using namespace std;
+int main()
+{
+  int arr[] = {1, 2, 3, 4, 5, 6};
+  int n = 6;
+  for (int st = 0; st < n; st++)
+  {
+    for (int end = st; end < n; end++)
+    {
+      for (int i = st; i <= end; i++)
+      {
+        cout << arr[i];
+      }
+      cout << " ";
+    }
+    cout << endl;
+  }
+  return 0;
+}
--- SHRADHA DIDI/ARRAYS/kadanesAlgorithm.cpp
+++ SHRADHA DIDI/ARRAYS/kadanesAlgorithm.cpp
@@ -1,37 +1,36 @@
-// // kadanesAlgorithm
-
 // #include <iostream>
 // using namespace std;
 // int main()
 // {
 //   int arr[] = {3, -4, 5, 4, -1, 7, -8};
 //   int n = 7;
-//   int maxSum = INT_MIN;
+//   int maxx = INT_MIN;
 //   int currentSum = 0;
 //   for (int i = 0; i < n; i++)
 //   {
 //     currentSum += arr[i];
-//     maxSum = max(currentSum, maxSum);
+//     maxx = max(currentSum, maxx);
 //     if (currentSum < 0)
 //     {
 //       currentSum = 0;
 //     }
 //   }
-//   cout << maxSum << endl;
+//   cout << maxx;
 //   return 0;
 // }
+
 #include <iostream>
 using namespace std;
 int main()
 {
   int arr[] = {3, -4, 5, 4, -1, 7, -8};
-  int n = 7;
-  int maxx = INT_MIN;
+  int n = 6;
+  int maxx INT_MIN;
   int currentSum = 0;
   for (int i = 0; i < n; i++)
   {
     currentSum += arr[i];
-    maxx = max(currentSum, maxx);
+    maxx = max(maxx, currentSum);
     if (currentSum < 0)
     {
       currentSum = 0;
