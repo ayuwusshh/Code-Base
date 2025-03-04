b31fb3c4dabfcfde3166ec451f75b89b26dbb507
AYUSH KUMAR SINGH <145869549+ayuwusshh@users.noreply.github.com>
2025-03-05 00:43:52 +0530

    ok

---
+++ sherians/Express/script.js
@@ -0,0 +1,6 @@
+// Express js tutorial
+/*
+Express js is a framework and a n npm package 
+framework--> gives a flow to use an existing tech
+it manages everyhting from receiveing the request and giving response
+*/
