562e7232db052568312d4524c6e61a4ab704a04b
AYUSH KUMAR SINGH <145869549+ayuwusshh@users.noreply.github.com>
2025-03-18 19:35:49 +0530

    fv

--- sherians/mini/index.js
+++ sherians/mini/index.js
@@ -1,12 +1,18 @@
 const express = require('express');
 const app = express();
-const path=require('path');
+const path = require('path');
 app.use(express.json());
 app.use(express.urlencoded({ extended: true }));
-app.use(express.static(path.join(__dirname,'public')));
+app.use(express.static(path.join(__dirname, 'public')));
 app.set('view engine', 'ejs')
 app.get("/", function (req, res) {
   res.render("index");
+})
+app.get("/profile/:Username", function (req, res) {
+  res.send(req.params.Username);
+})
+app.get("/profile/:Username/:age", function (req, res) {
+  res.send(`Name ${req.params.Username}, Age ${req.params.age}`);
 })
 app.listen(3000, function () {
   console.log('Its running');
