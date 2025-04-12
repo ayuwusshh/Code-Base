d613ad3d70e701e05734b5319b9b3850c4a0b5c7
AYUSH KUMAR SINGH <145869549+ayuwusshh@users.noreply.github.com>
2025-04-12 22:40:12 +0530

    project completed

--- Backend/Min_P1/app.js
+++ Backend/Min_P1/app.js
@@ -93,6 +93,10 @@ app.get("/like/:id", isLoggedIn, async (req, res) => {
 });
 app.get("/edit/:id", isLoggedIn, async (req, res) => {
   let post = await postModel.findOne({ _id: req.params.id }).populate("user");
-  
+  res.render("edit", { post })
+});
+app.post("/update/:id", isLoggedIn, async (req, res) => {
+  let post = await postModel.findOneAndUpdate({ _id: req.params.id }, { content: req.body.content })
+  res.redirect("/profile")
 });
 app.listen(3000); 
